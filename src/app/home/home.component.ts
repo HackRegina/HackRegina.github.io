d97287a5a3ce3f5f7ec159d88b9130efa4936280
Lucas Tétreault <lucas@vivvo.com>
2018-04-01 22:54:29 -0600

    feat: Use scss and fix the build!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { Sponsor } from '../sponsors/sponsor'
 import { SponsorsService } from '../sponsors/sponsors.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
   sponsors: Sponsor[] = []
 
-  constructor(private sponsorsService: SponsorsService) { }
+  constructor (private sponsorsService: SponsorsService) { }
 
-  ngOnInit() {
+  ngOnInit () {
     this.sponsorsService.find()
       .subscribe((s: Sponsor[]) => this.sponsors = s.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase())))
   }
 
-  getFilteredSponsors(query) {
+  getFilteredSponsors (query) {
     return query ? this.sponsors.filter(s => Object.keys(query).every(k => s[k] == query[k])) : this.sponsors
   }
 
