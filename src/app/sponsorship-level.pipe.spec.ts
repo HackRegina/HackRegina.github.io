b246fe5774861adb1d67cc3d5fb93fc4ade94fec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-05 04:59:19 +0000

    Deploy HackRegina/HackRegina.github.io to github.com/HackRegina/HackRegina.github.io.git:master

--- src/app/sponsorship-level.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SponsorshipLevelPipe } from './sponsorship-level.pipe';
-
-describe('SponsorshipLevelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SponsorshipLevelPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
