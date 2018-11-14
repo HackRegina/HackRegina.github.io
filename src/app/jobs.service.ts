c2b074ca8a5f13248200ed55513d267c99356aed
Lucas Tétreault <lucastetreault@gmail.com>
2018-11-13 21:16:20 -0600

    feat: Ignore jobs that were posted more than 3 months ago

--- src/app/jobs.service.ts
+++ src/app/jobs.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { Job } from './job'
+import * as moment from 'moment'
 
 @Injectable()
 export class JobsService {
@@ -9,7 +10,8 @@ export class JobsService {
   constructor (private http: HttpClient) { }
 
   getJobs (): Observable<Job[]> {
-    return this.http.get<Job[]>('https://api.github.com/repos/hackregina/jobs/issues')
+    let since = moment().subtract(3, 'months').format("YYYY-MM-DDTHH:MM:SSZ")
+    return this.http.get<Job[]>(`https://api.github.com/repos/hackregina/jobs/issues?state=open&since=${since}`)
   }
 
   getJob (num): Observable<Job> {
