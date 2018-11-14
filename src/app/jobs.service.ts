f7e5f587e5739a767bbc73f4e17420d1ad71724f
Lucas Tétreault <lucastetreault@gmail.com>
2018-11-13 22:15:11 -0600

    chore: Don't need to worry about stale date, it will close issues 7 days after it marks them stale

--- src/app/jobs.service.ts
+++ src/app/jobs.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { Job } from './job'
-import * as moment from 'moment'
 
 @Injectable()
 export class JobsService {
@@ -10,8 +9,7 @@ export class JobsService {
   constructor (private http: HttpClient) { }
 
   getJobs (): Observable<Job[]> {
-    let since = moment().subtract(3, 'months').format("YYYY-MM-DDTHH:MM:SSZ")
-    return this.http.get<Job[]>(`https://api.github.com/repos/hackregina/jobs/issues?state=open&since=${since}`)
+    return this.http.get<Job[]>(`https://api.github.com/repos/hackregina/jobs/issues?state=open`)
   }
 
   getJob (num): Observable<Job> {
